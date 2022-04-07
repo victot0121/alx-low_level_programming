26022607360ee850ff5763edb18f19c383653cbe
victor <victoroyiga01@gmail.com>
2022-04-07 04:57:48 -1100

    first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,6 @@
 */
 int main()
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
 	return (1);
 }

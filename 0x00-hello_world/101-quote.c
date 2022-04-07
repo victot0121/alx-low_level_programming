53c8d4ad63f47cbc801dca309695cacdd4e83cf6
victor <victoroyiga01@gmail.com>
2022-04-07 04:53:10 -1100

    first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
- * main - Entry point
- * Return:1
- */
-int main(void)
+/*
+ *main code
+*/
+int main()
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(ch);
-return (1);
+  write("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	return (1);
 }

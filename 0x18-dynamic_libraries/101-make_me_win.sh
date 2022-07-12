fe013408567a372743213dee124f3f768ca044da
victor <victoroyiga01@gmail.com>
2022-07-12 20:30:07 +0100

    m

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp/ https://raw.github.com/cas1908/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
--- 0x18-dynamic_libraries/README.md
+++
@@ -1,1 +0,0 @@
-Learning about dynamic libraries in C
